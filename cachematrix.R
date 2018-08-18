68962bcf5b74b1d4dbda6049b516c38ecbb02b50
shash123khr <shash123khr@gmail.com>
2018-08-18 22:37:05 +0530

    second commit

--- cachematrix.R
+++ cachematrix.R
@@ -3,18 +3,18 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-inver<-NULL
-set<-function(y)
+makeCacheMatrix <- function(x = matrix()) {  ## define the argument with default mode of "matrix"
+inver<-NULL ## initialize inver as NULL
+set<-function(y) ## define the set function to assign new
 {
-  x<<-y
+  x<<-y  #value of matrix in parent environment
   inver<<-NULL
 }
 get<-function()x
-
-setinverse<-function(inverse) inver<<-inver
-getinverse<-function()inver
-list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+ 
+setinverse<-function(inverse) inver<<-inver  ## assigns value of inv in parent environment
+getinverse<-function()inver  ## gets the value of inv where called
+list(set = set, get = get, setinverse = setinverse, getinverse = getinverse) ## you need this in order to refer to the functions with the $ operator
 }
 
 
